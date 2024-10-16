5288e34ced6548dddab140327cc9839e8ff865bd
zartyblartfast <zarty_blartfast@yahoo.com>
2024-10-16 21:59:56 +0100

    Update main.js

--- main.js
+++ main.js
@@ -116,7 +116,7 @@ document.addEventListener('DOMContentLoaded', function () {
       const hourAngles = calculateHourAngles(latitude);
 
       // Log the calculated hour angles
-      console.log('Calculated Hour Angles:', hourAngles);
+      //console.log('Calculated Hour Angles:', hourAngles);
 
       // Add logic here to use the hour angles for sundial rendering
     }
